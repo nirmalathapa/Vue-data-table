7f88e1894306cd81d2d6e6e3748a26e9e3a20032
nirmala <nirmala@surf.net.np>
2022-05-22 21:38:52 +0200

    Add index css and create userTable component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UserTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserTable from "./components/UserTable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    UserTable,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/UserTable.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "UserTable",
+  props: {},
+};
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Users table</h1>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
