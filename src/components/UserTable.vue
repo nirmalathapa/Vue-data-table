6f1219c9dc95edc5ea5b04acd69248619be930e1
nirmala <nirmala@surf.net.np>
2022-05-24 00:23:17 +0200

    Add axios and populate data

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -1,22 +1,18 @@
 <script>
+import axios from "axios";
+
 export default {
   name: "UserTable",
   data() {
     return {
-      tableHeaders: ["User Number", "User Name", "City", "Zip Code", "Address"],
-      tableData: [
-        {
-          id: 1,
-          name: "Leanne Graham",
-          city: "California",
-        },
-        {
-          id: 2,
-          name: "Sunny Sunshine",
-          city: "Copenhagen",
-        },
-      ],
+      tableHeaders: ["ID", "Name", "Username", "Email", "Phone", "Website"],
+      tableData: [],
     };
+  },
+  created: function () {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      return (this.tableData = response.data);
+    });
   },
   props: {},
 };
@@ -34,7 +30,36 @@ export default {
       </t-tag>
 
       <t-tag tag-name="div" class="my-8">
-        <t-table :headers="tableHeaders" :data="tableData"></t-table>
+        <t-table :headers="tableHeaders" :data="tableData">
+          <template slot="tbody" slot-scope="props">
+            <tbody
+              :class="props.tbodyClass"
+              v-for="item in tableData"
+              :key="item.id"
+            >
+              <tr :class="props.trClass">
+                <td :class="props.tdClass" colspan="1">
+                  {{ item.id }}
+                </td>
+                <td :class="props.tdClass" colspan="1">
+                  {{ item.name }}
+                </td>
+                <td :class="props.tdClass" colspan="1">
+                  {{ item.username }}
+                </td>
+                <td :class="props.tdClass" colspan="1">
+                  {{ item.email }}
+                </td>
+                <td :class="props.tdClass" colspan="1">
+                  {{ item.phone }}
+                </td>
+                <td :class="props.tdClass" colspan="1">
+                  {{ item.website }}
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </t-table>
       </t-tag>
     </t-tag>
   </div>
